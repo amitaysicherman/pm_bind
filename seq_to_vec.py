c59ea048dbfe7f4a0589ea0589845a7f57375258
amitay.s <amitay.sich@gmail.com>
2025-02-09 11:00:46 +0200

    start

--- seq_to_vec.py
+++ seq_to_vec.py
@@ -77,6 +77,7 @@ class ChemBERTa:
         self.model = AutoModel.from_pretrained("seyonec/ChemBERTa-zinc-base-v1").to(device).eval()
 
     def to_vec(self, seq_list: str):
+        print(list(seq_list))
         inputs = self.tokenizer(seq_list, return_tensors='pt', padding="longest", truncation=True, max_length=512)
         inputs = {k: v.to(device) for k, v in inputs.items()}
         with torch.no_grad():
